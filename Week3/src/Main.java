39c270f3b588684cb61d21953a184a30b19b7bdf
Magnus Muru <murumagnus@gmail.com>
2022-02-14 22:44:32 +0200

    Updated Grapevine. Right answer, cannot run in time

--- Week3/src/Main.java
+++ Week3/src/Main.java
@@ -1,43 +1,39 @@
 import java.util.Arrays;
-import java.util.OptionalInt;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 class Main {
 
     void Begin() {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
-            int[] intArray = Arrays.stream(scanner.nextLine().trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            int[] intArray = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
-            if (intArray[0] == 0 && intArray[1] == 0) {
+            int height = intArray[0];
+            int width = intArray[1];
+
+            if (height == 0) {
                 break;
             }
 
-            int[][] map = new int[intArray[0]][intArray[1]];
-            for (int i = 0; i < intArray[0]; i++) {
+            int[][] map = new int[height][width];
+            for (int i = 0; i < height; i++) {
 
-                map[i] = Arrays.stream(scanner.nextLine().trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+                map[i] = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
             }
-            int queryCount = Integer.parseInt(scanner.nextLine().trim());
+            int queryCount = Integer.parseInt(scanner.nextLine());
             for (int i = 0; i < queryCount; i++) {
-                int[] query = Arrays.stream(scanner.nextLine().trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+                int[] query = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
                 int currentMax = 0;
-                for (int j = 0; j < intArray[0]; j++) {
-                    OptionalInt optionalIndex = IntStream.of(map[j]).filter(x -> x >= query[0] && x <= query[1]).findFirst();
-                    if (!optionalIndex.isPresent()) {
-                        continue;
-                    }
+                for (int j = 0; j < height; j++) {
 
-                    int index = Arrays.binarySearch(map[j], optionalIndex.getAsInt());
-
-
-                    if (currentMax >= intArray[0] - j) {
+                    if (currentMax >= height - j) {
                         break;
                     }
 
-                    for (int k = 0; k < intArray[1]; k++) {
-                        if (index + k >= intArray[1] || i + k >= intArray[0] || map[i + k][index + k] > query[1]) {
+                    int index = lower(map[j], query[0]);
+
+                    for (int k = 0; k < width; k++) {
+                        if (j + k >= height ||index + k >= width || map[j + k][index + k] > query[1]) {
                             break;
                         }
                         if (k + 1 > currentMax) {
@@ -49,6 +45,22 @@ class Main {
             }
             System.out.println("-");
         }
+    }
+
+    static int lower(int[] array, int key)
+    {
+        int lowerBound = 0;
+
+        while (lowerBound < array.length) {
+
+            if (key > array[lowerBound])
+                lowerBound++;
+
+            else
+                return lowerBound;
+        }
+
+        return lowerBound;
     }
 
     public static void main(String[] args) {
